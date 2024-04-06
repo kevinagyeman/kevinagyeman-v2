0dfc7cc6d9083397e1d588f61a3c776b6baa8da4
Kevin Agyeman <kevinagyemann@gmail.com>
2024-04-06 00:00:21 -0400

    test project

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,11 +1,26 @@
 import Hero from '@/components/hero.component';
 import ProjectsListUser from '@/components/project/projects-list-user.component';
+import { ProjectSchema } from '@/types/project-schema';
+import { getProjects } from '@/utils/utils';
 
-export default function Index() {
+export default async function Index() {
+  const projectsList: ProjectSchema[] | undefined = await getProjects(
+    {
+      fieldPath: 'createdAt',
+      directionStr: 'desc',
+    },
+    {
+      fieldPath: 'isPublished',
+      opStr: '==',
+      value: true,
+    }
+  );
   return (
     <>
       <Hero />
-      <ProjectsListUser />
+      {projectsList ? (
+        <ProjectsListUser projects={JSON.parse(JSON.stringify(projectsList))} />
+      ) : null}
     </>
   );
 }
--- src/components/project/projects-list-user.component.tsx
+++ src/components/project/projects-list-user.component.tsx
@@ -1,22 +1,17 @@
+'use client';
+
 import { getProjects, splitByLanguage, splitSkills } from '@/utils/utils';
 import { ArrowUpRight, Check } from 'lucide-react';
 import { getTranslations } from 'next-intl/server';
 import { Link } from '../../../navigation';
 import { ProjectSchema } from '../../types/project-schema';
+import { useTranslations } from 'next-intl';
 
-export default async function ProjectsListUser() {
-  const projects: ProjectSchema[] | undefined = await getProjects(
-    {
-      fieldPath: 'createdAt',
-      directionStr: 'desc',
-    },
-    {
-      fieldPath: 'isPublished',
-      opStr: '==',
-      value: true,
-    }
-  );
-  const t = await getTranslations('index');
+type ProjectsListUserProps = {
+  projects: ProjectSchema[];
+};
+export default function ProjectsListUser({ projects }: ProjectsListUserProps) {
+  const t = useTranslations('index');
 
   return (
     <>
@@ -24,22 +19,27 @@ export default async function ProjectsListUser() {
         {t('projects.title')}
       </h2>
       <p className='text-muted-foreground'>{t('projects.description')}</p>
-      {projects?.map(async (project: ProjectSchema, index: number) => (
-        <Link href={`/project/${project.id}`} key={index} rel='canonical'>
+      {projects?.map((project: ProjectSchema, index: number) => (
+        <Link
+          href={`/project/${project.id}`}
+          key={index}
+          rel='canonical'
+          prefetch={true}
+        >
           <div
             className='my-3 flex 
     flex-col space-y-3 rounded-lg border p-6 lg:transition lg:ease-in-out lg:hover:scale-110 lg:hover:bg-zinc-100 lg:dark:hover:bg-zinc-900'
           >
             <div className='flex'>
               <h3 className='truncate text-2xl font-semibold'>
-                {await splitByLanguage(`${project.title}`)}
+                {`${project.title}`}
               </h3>
               <div className='ml-auto'>
                 <ArrowUpRight />
               </div>
             </div>
             <p className='line-clamp-2 text-muted-foreground'>
-              {await splitByLanguage(`${project.shortDescription}`)}
+              {`${project.shortDescription}`}
             </p>
             <div className='flex flex-wrap gap-x-3 gap-y-1'>
               {splitSkills(`${project?.skills}`, 3).map((skill, index) => (

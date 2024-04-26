2655775a73ba1c5bc8f9dd28046ee52720e8623b
Kevin Agyeman <kevinagyemann@gmail.com>
2024-04-26 19:42:16 -0400

    fixed links iteration

--- src/components/link-input.component.tsx
+++ src/components/link-input.component.tsx
@@ -1,71 +1,54 @@
-import React, { useEffect, useState } from 'react';
-import { Label } from './ui/label';
-import { Input } from './ui/input';
-import { Button } from './ui/button';
-import { Minus, Plus } from 'lucide-react';
-import { useRecoilState } from 'recoil';
-import { Link, ProjectSchema } from '@/types/project-schema';
 import { projectDataState } from '@/store/projects-store';
+import { Link, ProjectSchema } from '@/types/project-schema';
+import { Minus } from 'lucide-react';
+import React, { useEffect, useState } from 'react';
+import { useRecoilState } from 'recoil';
+import { Button } from './ui/button';
+import { Input } from './ui/input';
+import { Label } from './ui/label';
 
 export default function LinkInput() {
   const [project, setProject] = useRecoilState<ProjectSchema>(projectDataState);
-  const [links, setLinks] = useState<Link[]>(
-    project.links ? project.links : []
-  );
-
-  const handleAddLink = () => {
-    const newLink: Link = { label: '', url: '' };
-    setLinks([...links, newLink]);
-  };
-
-  const handleRemoveLink = (index: number) => {
-    const updatedLinks: Link[] = [...links];
-    updatedLinks.splice(index, 1);
-    setLinks(updatedLinks);
-  };
-
-  const handleLabelChange = (
-    event: React.ChangeEvent<HTMLInputElement>,
-    index: number
-  ) => {
-    const updatedLinks: Link[] = [...links];
-    updatedLinks[index].label = event.target.value;
-    setLinks(updatedLinks);
-  };
-
-  const handleUrlChange = (
-    event: React.ChangeEvent<HTMLInputElement>,
-    index: number
-  ) => {
-    const updatedLinks: Link[] = [...links];
-    updatedLinks[index].url = event.target.value;
-    setLinks(updatedLinks);
-  };
-
-  useEffect(() => {
-    console.log(project.links);
-  }, [project]);
-
   return (
     <>
       <Label>Link Input</Label>
       <div className='flex flex-col gap-3'>
-        {links.map((link: Link, index: number) => (
+        {project.links?.map((link: Link, index: number) => (
           <div key={index}>
             <div className='flex flex-row gap-3 items-center p-4 bg-secondary rounded-lg mt-2'>
               <div className='flex flex-col gap-y-2 flex-auto'>
+                <Label className='text-xs'>Label</Label>
                 <Input
                   type='text'
                   placeholder='Label link'
                   value={link.label}
-                  onChange={(event) => handleLabelChange(event, index)}
+                  onChange={(event) =>
+                    setProject({
+                      ...project,
+                      links: project?.links?.map((item: Link) =>
+                        item.label === link.label
+                          ? { ...item, label: event.target.value }
+                          : item
+                      ),
+                    })
+                  }
                   required
                 />
+                <Label className='text-xs'>Link</Label>
                 <Input
                   type='text'
                   placeholder='Link'
                   value={link.url}
-                  onChange={(event) => handleUrlChange(event, index)}
+                  onChange={(event) =>
+                    setProject({
+                      ...project,
+                      links: project?.links?.map((item: Link) =>
+                        item.url === link.url
+                          ? { ...item, url: event.target.value }
+                          : item
+                      ),
+                    })
+                  }
                   required
                 />
               </div>
@@ -73,7 +56,14 @@ export default function LinkInput() {
                 size='icon'
                 className='rounded-full'
                 type='button'
-                onClick={() => handleRemoveLink(index)}
+                onClick={() =>
+                  setProject({
+                    ...project,
+                    links: project?.links?.filter(
+                      (item: Link) => item.url !== link.url
+                    ),
+                  })
+                }
               >
                 <Minus className='h-4 w-4' />
               </Button>
@@ -83,17 +73,15 @@ export default function LinkInput() {
         <div>
           <Button
             type='button'
-            onClick={() => handleAddLink()}
+            onClick={() => {
+              setProject({
+                ...project,
+                links: [...(project?.links || []), { label: '', url: '' }],
+              });
+            }}
             variant={'secondary'}
           >
             Add New Link
-          </Button>
-          <Button
-            type='button'
-            onClick={() => setProject({ ...project, links: links })}
-            variant={'secondary'}
-          >
-            confirm
           </Button>
         </div>
       </div>
